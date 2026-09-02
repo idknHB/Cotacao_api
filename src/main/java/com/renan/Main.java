243ebd504d0fd82ea298dd5c8e920011764361f0
idknHB <renanhammerschlagbueno@gmail.com>
2026-09-02 09:45:49 -0300

    feat: move conversion logic to dedicated method
    Add automatic currency convresion, and removed conversion button for better usability

--- src/main/java/com/renan/Main.java
+++ src/main/java/com/renan/Main.java
@@ -24,22 +24,76 @@ import java.util.Currency;
 
 public class Main extends Application {
 
+    private ComboBox<Moeda> moedaOrigem;
+    private ComboBox<Moeda> moedaDestino;
+    private TextField txtValor;
+    private Label resultado;
+    private void converter(){
+        try{
+            Moeda origem = moedaOrigem.getValue();
+            Moeda destino = moedaDestino.getValue();
+
+            if(origem == null || destino == null){
+                return;
+            }
+
+            if(txtValor.getText().isEmpty()){
+                return;
+            }
+
+            double valor =
+                    Double.parseDouble(txtValor.getText());
+
+            ApiCliente api = new ApiCliente();
+
+            String json =
+                    api.buscarCotacao(
+                            origem.getCodigo()
+                    );
+
+            Gson gson = new Gson();
+
+            CurrencyResponse response =
+                    gson.fromJson(
+                            json,
+                            CurrencyResponse.class
+                    );
+
+            double taxa =
+                    response.getRates()
+                            .get(destino.getCodigo());
+
+            double convertido =
+                    valor * taxa;
+
+            resultado.setText(
+                    String.format(
+                            "$%.2f",
+                            convertido
+                    )
+            );
+
+        }catch(Exception e){
+            resultado.setText(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
     @Override
     public void start(Stage stage) throws IOException, InterruptedException {
 
         Label titulo = new Label("Cotação de Moedas");
-        Label resultado = new Label("Nenhuma consulta realizada");
+        resultado = new Label("Nenhuma consulta realizada");
         Label lblOrigem = new Label("Moeda de origem");
         Label lblDestino = new Label("Moeda de destino");
         Label lblValor = new Label("Valor");
-        Button btnConverter = new Button("Converter");
         Button btnTrocar = new Button("\uD83D\uDD04");
-        TextField txtValor = new TextField();
+        txtValor = new TextField();
 
-        ComboBox<Moeda> moedaOrigem =
+        moedaOrigem =
                 new ComboBox<>();
 
-        ComboBox<Moeda> moedaDestino =
+        moedaDestino =
                 new ComboBox<>();
 
         ApiCliente api = new ApiCliente();
@@ -79,39 +133,17 @@ public class Main extends Application {
            );
        }
 
-        btnConverter.setOnAction(event -> {
-            try {
-                String origem =
-                        moedaOrigem.getValue().getCodigo();
+       moedaOrigem.setOnAction(event ->
+               converter()
+       );
+       moedaDestino.setOnAction(event ->
+               converter()
+       );
 
-                String destino =
-                        moedaDestino.getValue().getCodigo();
-
-                String jsonAtual =
-                        api.buscarCotacao(origem);
-
-                CurrencyResponse responseAtual =
-                        gson.fromJson(
-                        jsonAtual,
-                        CurrencyResponse.class);
-
-                double valor = Double.parseDouble(txtValor.getText());
-                double taxa = responseAtual.getRates().get(destino);
-                double convertido = valor * taxa;
-
-                resultado.setText(
-                        String.format(
-                                "%.2f",
-
-                                convertido
-                        )
-                );
-
-            }catch (Exception ex){
-                resultado.setText(ex.getMessage());
-                ex.printStackTrace();
-            }
-        });
+        txtValor.textProperty().addListener(
+                (obs, oldValue, newValue) ->
+                        converter()
+        );
 
        btnTrocar.setOnAction(event -> {
            Moeda temp = moedaOrigem.getValue();
@@ -120,6 +152,8 @@ public class Main extends Application {
                    moedaDestino.getValue());
 
            moedaDestino.setValue(temp);
+
+           converter();
        });
 
 
@@ -135,7 +169,6 @@ public class Main extends Application {
         moedaOrigem.setPrefWidth(300);
         moedaDestino.setPrefWidth(300);
         txtValor.setPrefWidth(300);
-        btnConverter.setPrefWidth(300);
 
         lblOrigem.setStyle("-fx-text-fill: white;");
         lblDestino.setStyle("-fx-text-fill: white;");
@@ -143,17 +176,14 @@ public class Main extends Application {
 
         titulo.setStyle(Styles.TITLE);
         resultado.setStyle(Styles.RESULT);
-        btnConverter.setStyle(Styles.BUTTON);
+
         card.setStyle(Styles.CARD);
 
         moedaOrigem.setPrefHeight(35);
         moedaDestino.setPrefHeight(35);
         txtValor.prefHeight(35);
-        btnConverter.prefHeight(55);
-        btnConverter.setMaxWidth(250);
 
         shadow.setRadius(15);
-        btnConverter.setEffect(shadow);
 
         cardResultado.setStyle(
                 "-fx-background-color: #1A1A1A;" +
@@ -169,7 +199,6 @@ public class Main extends Application {
                 btnTrocar,
                 moedaDestino,
                 txtValor,
-                btnConverter,
                 cardResultado
         );
         Scene scene = new Scene(layout, 800, 600);
